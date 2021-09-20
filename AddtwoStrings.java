91185da1c1b93d54432d1f1db2dc7ea661a42710
teenu19012287 <73779829+teenu19012287@users.noreply.github.com>
2021-09-20 16:04:08 +0530

    Create AddtwoStrings.java
     WAP to add two strings in java?
     Integer.parseInt(s1) will convert string to integer

---
+++ AddtwoStrings.java
@@ -0,0 +1,18 @@
+/* WAP to add two strings in java?
+ * string a  = "111";
+ * string b = "222";
+ * output:"333";
+ * 
+ */
+
+public class AddtwoStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            String s1 = "121";
+            String s2 = "242";
+        int sum =     Integer.parseInt(s1)+Integer.parseInt(s2);
+          System.out.println(sum);  
+	}
+
+}
