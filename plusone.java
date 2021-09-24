ab8f59bb449dd0da514ae3b384af389f1ea2f41f
teenu19012287 <73779829+teenu19012287@users.noreply.github.com>
2021-09-24 09:40:10 +0530

    Create plusone.java
    You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
    
    Increment the large integer by one and return the resulting array of digits.
    
     
    
    Example 1:
    
    Input: digits = [1,2,3]
    Output: [1,2,4]
    Explanation: The array represents the integer 123.
    Incrementing by one gives 123 + 1 = 124.
    Thus, the result should be [1,2,4].
    Example 2:
    
    Input: digits = [4,3,2,1]
    Output: [4,3,2,2]
    Explanation: The array represents the integer 4321.
    Incrementing by one gives 4321 + 1 = 4322.
    Thus, the result should be [4,3,2,2].

---
+++ plusone.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+       int n = digits.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i] == 9){
+                   digits[i] =0;
+            }
+            else{
+                digits[i]+=1;
+                return digits;
+            }
+            
+        }
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+        return arr;
+    }}
+
+
